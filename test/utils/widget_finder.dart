996d636920e76bba66a7e876ac058fdd5849997f
kartikaeyy <kartikeyai246@gmail.com>
2026-03-29 22:38:49 +0530

    minor fix

--- test/utils/widget_finder.dart
+++ test/utils/widget_finder.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:paintroid/core/utils/widget_identifier.dart';
